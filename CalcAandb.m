b2fa3290b18ed74fdc79afc2c765d7abb063d19d
to232 <tyceolaveson@gmail.com>
2024-02-21 10:14:50 -0700

    Adding comments and testing

--- CalcAandb.m
+++ CalcAandb.m
@@ -1,7 +1,10 @@
-function [outputArg1,outputArg2] = CalcAandb(inputArg1,inputArg2)
-%CALCAANDB Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [A,b] = CalcAandb(n,gamma)
+%This script will calculate the matrix A and b for the GMRES problem
+% Input: n - the number of basis functions, gamma the value of the
+% potential: V(x) = gamma.
+% Output: A, a tridiagonal matrix with elements as defined in part b
+% b - the source vector.
+
+
 end
 
